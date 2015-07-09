beb3a9a68270dbccf8e57b7199b6123a638bed2e
christian.tomyama <christian.tomyama@Christian.gazin.com.br>
2015-07-09 14:35:26 -0300

    a

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ and open the template in the editor.
         <meta charset="ISO-8859-1">
         <title></title>
     </head>
-    <body>ZZZZZZZZZZZ
+    <body>ZZZZZ
         <?php
         // put your code here
         ?>
